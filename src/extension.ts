ce9c46253cc3947e012bdfe51b5b91712fea86f0
An Phi <aphi@skidmore.edu>
2024-07-11 12:23:36 -0400

    minor improvement to prevent space buildup

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { exec, execSync } from 'child_process';
 import { existsSync } from 'fs';
 import {
   window,
@@ -8,23 +8,56 @@ import {
   type TerminalProfile,
   type ProviderResult,
   workspace,
+  ProgressLocation,
+  commands,
 } from 'vscode';
 
 const REPL_TERMINAL_NAME = 'Legend REPL (test)';
 
+async function downloadReplJar(
+  context: ExtensionContext,
+  version: string,
+  replDir: string,
+  replJARPath: string,
+) {
+  execSync(
+    `rm -rf ${context.asAbsolutePath(
+      replDir,
+    )} && mkdir ${context.asAbsolutePath(replDir)}`,
+  );
+  const replJarDownloadUrl = (
+    workspace.getConfiguration('repl').get('jarDownloadUrl') as string
+  ).replace('{{version}}', version);
+  await window.withProgress(
+    {
+      location: ProgressLocation.Notification,
+      title: `Downloading REPL JAR from: ${replJarDownloadUrl}`,
+    },
+    (progress) => {
+      return new Promise((resolve, reject) => {
+        exec(
+          `curl -L ${replJarDownloadUrl} -o ${context.asAbsolutePath(
+            replJARPath,
+          )}`,
+          (error) => {
+            if (!error) {
+              resolve('');
+            } else {
+              reject(error);
+            }
+          },
+        );
+      });
+    },
+  );
+}
+
 export async function activate(context: ExtensionContext) {
   const version = context.extension.packageJSON.version;
-  const replJar = `legend-repl-${version}.jar`;
-  if (!existsSync(context.asAbsolutePath(replJar))) {
-    const replJarDownloadUrl = (
-      workspace.getConfiguration('repl').get('jarDownloadUrl') as string
-    ).replace('{{version}}', version);
-    window.showInformationMessage(
-      `Downloading REPL JAR from: ${replJarDownloadUrl} ...`,
-    );
-    execSync(
-      `curl -L ${replJarDownloadUrl} -o ${context.asAbsolutePath(replJar)}`,
-    );
+  const replDir = `repl`;
+  const replJARPath = `${replDir}/legend-repl-${version}.jar`;
+  if (!existsSync(context.asAbsolutePath(replJARPath))) {
+    await downloadReplJar(context, version, replDir, replJARPath);
   }
 
   let port = 9006;
@@ -32,12 +65,10 @@ export async function activate(context: ExtensionContext) {
     port = await module.default();
   });
 
-  const provider = window.registerTerminalProfileProvider(
+  const terminalProfileProvider = window.registerTerminalProfileProvider(
     'legend-repl-test.terminal.repl',
     {
-      provideTerminalProfile(
-        token: CancellationToken,
-      ): ProviderResult<TerminalProfile> {
+      provideTerminalProfile(): ProviderResult<TerminalProfile> {
         return {
           options: {
             name: REPL_TERMINAL_NAME,
@@ -49,11 +80,16 @@ export async function activate(context: ExtensionContext) {
                   port.toString(),
                 )}`,
               `-Dlegend.repl.dataCube.devPort=${port}`,
-              `-Dlegend.repl.dataCube.gridLicenseKey=${workspace
-                .getConfiguration('repl')
-                .get('agGridLicense', '')}`,
+              Boolean(
+                workspace.getConfiguration('repl').get('agGridLicense'),
+              ) &&
+                `-Dlegend.repl.dataCube.gridLicenseKey=${
+                  workspace
+                    .getConfiguration('repl')
+                    .get('agGridLicense') as string
+                }`,
               '-jar',
-              context.asAbsolutePath(replJar),
+              context.asAbsolutePath(replJARPath),
             ].filter((val): val is string => Boolean(val)),
             message:
               `Starting ${REPL_TERMINAL_NAME}...\r\n\r\nType 'help' or hit 'Enter' to see the full list of commands and their usage.\r\n` +
@@ -70,7 +106,15 @@ export async function activate(context: ExtensionContext) {
     },
   );
 
-  context.subscriptions.push(provider);
+  context.subscriptions.push(terminalProfileProvider);
+
+  const reDownloadJarCommand = commands.registerCommand(
+    'legend-repl-test.command.refreshJar',
+    async () => {
+      await downloadReplJar(context, version, replDir, replJARPath);
+    },
+  );
+  context.subscriptions.push(reDownloadJarCommand);
 }
 
 export function deactivate(): void {
